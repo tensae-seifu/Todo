932937a375c2c0df14b5f0dfb86cb44146fdaf06
dinozoff1981 <tensaeg@yahoo.com>
2023-10-22 03:18:17 +0200

    Add project files.

---
+++ todo/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace todo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+
+            while (true)
+            {
+                Console.WriteLine("Choose what you wish to do");
+                Console.WriteLine("+ to ADD");
+                Console.WriteLine("L to list");
+                Console.WriteLine("N to See Next item");
+                char val = Convert.ToChar(Console.ReadLine());
+
+                switch (val)
+                {
+                    case '+':
+                        AddList();
+                        break;
+                    case 'L':
+                        GetList();
+                        break;
+                    case 'N':
+                        NextItem();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice");
+                        break;
+                }
+            }
+
+            void GetList()
+            {
+                foreach (string item in queue)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            void AddList()
+            {
+                string answer;
+
+                do
+                {
+                    Console.WriteLine("Lägg till to do");
+                    string todo = Console.ReadLine();
+                    queue.Enqueue(todo);
+                    Console.WriteLine("Would you like to add more? (y/n): ");
+                    answer = Console.ReadLine();
+
+                    if (answer.ToLower() != "y")
+                    {
+                        break; 
+                    }
+                } while (true);
+            }
+
+            void NextItem()
+            {
+                if (queue.Count > 0)
+                {
+                    Console.WriteLine(queue.Peek());
+                }
+                else
+                {
+                    Console.WriteLine("Queue is empty");
+                }
+            }
+        }
+    }
+}
